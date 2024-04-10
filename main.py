c60975dd2964e9420118eb6888d20431971be223
Tjimenez1303 <tjimenez@unal.edu.co>
2024-04-10 12:49:32 -0500

    Upate a JS

--- main.py
+++
@@ -1,9 +0,0 @@
-# Define tu función
-def my_function_vs(req, res):
-  print("Hola mundo desde GitHub Actions V2")
-
-  
-# Exporta la función como el punto de entrada
-exports = {
-  "my_function_vs": my_function_vs
-}
