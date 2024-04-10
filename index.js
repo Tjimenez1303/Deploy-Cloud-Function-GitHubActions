052255a0c7696a8f21bc6f309913650e1408df37
Tjimenez1303 <tjimenez@unal.edu.co>
2024-04-10 11:44:54 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Define tu función
 function myFunction(req, res) {
-  console.log("Hola mundo desde GitHubAction")
+  console.log("Hola mundo desde GitHubAction V2")
 }
 
 // Exporta la función como el punto de entrada
